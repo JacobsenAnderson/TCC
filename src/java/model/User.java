31a5748ee55398b0f403a26198a4fab7afcc466e
JACOBSEN Anderson Luiz <anderson.jacobsen@gmail.com>
2019-07-10 22:55:15 -0300

    Criado o repositorio

---
+++ src/java/model/User.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package model;
+
+import java.security.Timestamp;
+
+/**
+ *
+ * @author Anderson
+ */
+public class User {
+    
+    private Integer user_id;
+    private String login;
+    private String name;
+    private String number;
+    private String password;
+    private String mail;
+    private String perfil;
+    private Timestamp bdate;
+    
+    
+    
+    
+    
+    
+}
